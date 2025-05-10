a83b66b5806862b6f739acadd2deac9bbd1ca2b8
diegofernando19 <diego.fernando.sanchez19@gmailcom>
2025-05-10 19:22:17 -0300

    clase 2 - 20250430

---
+++ variables-tipo-dato.py
@@ -0,0 +1,63 @@
+# ## Variables enteras
+# int_1 = 10
+# int_2 = 2
+
+# # suma
+# suma = int_1 + int_2
+# print(f"La suma entre {int_1} y {int_2} es: {suma}")
+
+# # resta
+# resta = int_1 - int_2
+# print(f"La resta entre {int_1} y {int_2} es: {resta}")
+
+# # multipliacion
+# multipliacion = int_1 * int_2
+# print(f"La multipliacion entre {int_1} y {int_2} es: {multipliacion}")
+
+# # division
+# division = int_1 / int_2
+# print(f"La division entre {int_1} y {int_2} es: {division}")
+
+# # potencia
+# potencia = int_1 ** int_2
+# print(f"La potencia entre {int_1} y {int_2} es: {potencia}")
+
+# ## Variables string
+# str_1 = "Info"
+# str_2 = "rmatorio"
+
+# suma = str_1 + str_2
+# print(f"La suma entre {str_1} y {str_2} es: {suma}")
+
+
+# ## Variables listas
+# lis_1 = [1, 2, 3]
+# lis_2 = [4, 5, 6]
+
+# # Operadores
+# suma = lis_1 + lis_2
+# print(f"La suma entre {lis_1} y {lis_2} es: {suma}")
+
+# # resta NO SE PUEDE RESTAR
+# resta = lis_1 - lis_2
+# print(f"La resta entre {lis_1} y {lis_2} es: {resta}")
+
+# lista = [10,5,15.30,2,4,11,22,1,55,3,99]
+# # utilizando .sort()
+# lista.sort()
+
+# print(lista)
+# lista = [10,20,30,40,40,50,]
+# print(lista[2])
+
+# lista = ['Gerardo', 'Andres', 'Ainara']
+# lista.sort()
+# print(lista)
+# lista.sort(reverse=True)
+# print(lista)
+
+# tupla = ('Gerardo', 'Andres', 'Ainara', 3, 2, 1)
+# print(tupla[4])
+
+diccionario = {'27438878': 'Diego Sanhcez', '2743879': 'Diego Fernando'}
+print(diccionario['27438878'])
