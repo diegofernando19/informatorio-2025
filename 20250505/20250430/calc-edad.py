325042ff2560dee09d16ac53e1d8797d32a45fb5
diegofernando19 <diego.fernando.sanchez19@gmail.com>
2025-05-21 15:32:00 -0300

    case 2 - 20250430

---
+++ 20250505/20250430/calc-edad.py
@@ -0,0 +1,15 @@
+anio_actual =2025
+mayoria_edad = 18
+
+anio_nac = int(input("Ingresar anio de nacimiento"))
+
+ania_usuario = anio_actual - anio_nac
+
+if (ania_usuario >= mayoria_edad):
+    print("El usuario es mayor de edad")
+
+else:
+    print("El Usuario NO es mayor de edad")
+
+
+
---
+++ 20250505/20250430/variable-tipo-dato.py
@@ -0,0 +1,57 @@
+# int_1 = 5
+# int_2 = 2
+
+# # suma
+# suma = int_1 + int_2
+# print(f"la suma entre {int_1} y {int_2} es: {suma}")
+
+# # resta
+# resta = int_1 - int_2
+# print(f"la resta entre {int_1} y {int_2} es: {resta}")
+
+# # multiplicacion
+# multiplicacion = int_1 * int_2
+# print(f"la multiplicacion entre {int_1} y {int_2} es: {multiplicacion}")
+
+# # division
+# division = int_1 / int_2
+# print(f"la division entre {int_1} y {int_2} es: {division}")
+
+# # potencia
+# potencia = int_1 ** int_2
+# print(f"la potencia entre {int_1} y {int_2} es: {potencia}")
+
+## Variables string
+# str_1 = "Info"
+# str_2 = "rmatorio"
+
+## Operadores
+# suma
+# suma = str_1 + str_2
+# print(f"la suma entre {str_1} y {str_2} es: {suma}")
+
+## Variables listas
+# lis_1 = [1, 2, 3]
+# lis_2 = [4, 5, 6]
+
+## Operadores
+# suma
+# suma = lis_1 + lis_2
+# print(f"la suma entre {lis_1} y {lis_2} es: {suma}")
+
+# lista = [1332, 8, 14, 13, 53, 30, 20]
+# lista.sort()
+# print(lista)
+
+# lista = [10, 20, 30, 40, 50]
+# print(lista[3])
+
+# lista = ["Matteo", "Paola", "Bety", "Diego"]
+# lista.sort()
+# print(lista)
+
+# tupla = ("Matteo", "Paola", "Diego")
+# print(tupla[2])
+
+diccionario = {"27438878" : "Diego Sanchez", "27438879" : "Diego Sancho"}
+print(diccionario["27438878"])
